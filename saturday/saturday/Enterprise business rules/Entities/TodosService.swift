cd127d41ba1fb5a493d8cabb3428ccd49249cda0
None <None@gmail.com>
2020-03-20 18:09:48 +0000

    Fetching remote JSON instead of local

--- saturday/saturday/Enterprise business rules/Entities/TodosService.swift
+++ saturday/saturday/Enterprise business rules/Entities/TodosService.swift
@@ -24,10 +24,11 @@ class TodosService {
 
     func retrieve(completionHandler: @escaping (_ todos: [String]) -> Void) {
 
-        guard let responseData = loadJson(filename: "todossmall") else {
-            completionHandler(["had some", "problems"])
-            return
-        }
+        retrieveRemote(completionHandler: completionHandler)
+    }
+
+    fileprivate func adaptJSONToArray(_ responseData: [TodosService.ResponseData],
+                                      completionHandler: @escaping ([String]) -> Void) {
         var titles = [String]()
         for row in responseData {
             titles.append(row.title)
@@ -35,5 +36,39 @@ class TodosService {
         DispatchQueue.global().async {
             completionHandler(titles)
         }
+    }
+
+    func retrieveLocal(completionHandler: @escaping (_ todos: [String]) -> Void) {
+
+        guard let responseData = loadJson(filename: "todossmall") else {
+            completionHandler(["had some", "problems"])
+            return
+        }
+        adaptJSONToArray(responseData, completionHandler: completionHandler)
+    }
+
+    enum Endpoints: String {
+        case todos = "https://jsonplaceholder.typicode.com/todos"
+    }
+
+    func retrieveRemote(completionHandler: @escaping (_ todos: [String]) -> Void) {
+        //request
+        // get json out of request
+        // return
+
+        let session: URLSession = URLSession(configuration: .default)
+        session.dataTask(with: URL(string: Endpoints.todos.rawValue)!, completionHandler: { data, _, error in
+            guard let responseData = data else {
+                completionHandler(["Something", "went", "wrong", error.debugDescription])
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode([ResponseData].self, from: responseData)
+                self.adaptJSONToArray(jsonData, completionHandler: completionHandler)
+            } catch {
+                print("error:\(error)")
+            }
+        }).resume()
     }
 }
