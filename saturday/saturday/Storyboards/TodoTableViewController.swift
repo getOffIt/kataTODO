b59643192f768e457b59120b5108b8eb996c8cfb
None <None@gmail.com>
2020-03-23 13:45:28 +0000

    filtering based on completion status

--- saturday/saturday/Storyboards/TodoTableViewController.swift
+++ saturday/saturday/Storyboards/TodoTableViewController.swift
@@ -18,34 +18,69 @@ class TodoViewController: UIViewController {
     }
 
     @IBAction func segmentControllerValueDidChange(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            todos.completionFilter = .all
+        case 1:
+            todos.completionFilter = .active
+        case 2:
+            todos.completionFilter = .completed
+        default:
+            todos.completionFilter = .all
+        }
+        tableView.reloadData()
     }
 }
 
-extension TodoViewController: UITableViewDataSource {
-    // MARK: - Table view data source
+    extension TodoViewController: UITableViewDataSource {
+        // MARK: - Table view data source
 
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todos.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TODOCELLreuseIdentifier", for: indexPath)
-
-        cell.textLabel?.text = todos[indexPath.row].title
-        return cell
-    }
-}
-
-class ElementProvider {
-    var todos = [TODOsPODO]([TODOsPODO(title: "Loading", userId: 0, completed: false)])
-    var count: Int {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             return todos.count
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TODOCELLreuseIdentifier", for: indexPath)
+
+            cell.textLabel?.text = todos[indexPath.row].title
+            return cell
+        }
     }
-    subscript(index: Int) -> TODOsPODO {
-        return todos[index]
+
+    enum CompletionFilter {
+        case all
+        case active
+        case completed
     }
+
+    class ElementProvider {
+        var todos = [TODOsPODO]([TODOsPODO(title: "Loading", userId: 0, completed: false)])
+        var completionFilter = CompletionFilter.all
+        lazy private var activeTodos = todos.filter { $0.completed == false }
+        lazy private var completedTodos = todos.filter { $0.completed == true }
+
+        var count: Int {
+            switch completionFilter {
+            case .all:
+                return todos.count
+            case .active:
+                return activeTodos.count
+            case .completed:
+                return completedTodos.count
+            }
+        }
+        subscript(index: Int) -> TODOsPODO {
+            switch completionFilter {
+            case .all:
+                return todos[index]
+            case .active:
+                return activeTodos[index]
+            case .completed:
+                return completedTodos[index]
+            }
+        }
 }
